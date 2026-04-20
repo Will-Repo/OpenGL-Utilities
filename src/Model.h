6a5767e08ff37e2ba856f27df337845b16dac553
Will <wricks2023.16@gmail.com>
2026-04-20 16:56:01 +0100

    Made meshes public

--- src/Model.h
+++ src/Model.h
@@ -11,8 +11,8 @@ class Model {
         Model() = default; // For cases where objects allocated immediately - must be overriden before use.
         Model(std::string path);
         void drawModel(unsigned int program);
-    private:
         std::vector<Mesh> meshes;
+    private:
         void loadModel(std::string path);
         void processNode(aiNode *node, const aiScene *scene);
         Mesh processMesh(aiMesh *mesh, const aiScene *scene);
